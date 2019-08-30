8df983dd6726e9c3803b5981af8d18a5c65c5d90
whyyougottabesorude <yossandev@gmail.com>
2019-08-30 22:18:59 +0700

    Updates

---
+++ precache-manifest.5db7a5bf936197aff1eea175a3bf8b2c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d374ebba4651af99d0a7ddbf81724d81",
+    "url": "/search-react/index.html"
+  },
+  {
+    "revision": "5c166d3f2d7d10e311c9",
+    "url": "/search-react/static/css/main.08242581.chunk.css"
+  },
+  {
+    "revision": "3547d44d933c02330ff6",
+    "url": "/search-react/static/js/2.cfdfcc2a.chunk.js"
+  },
+  {
+    "revision": "5c166d3f2d7d10e311c9",
+    "url": "/search-react/static/js/main.9c62c801.chunk.js"
+  },
+  {
+    "revision": "1f39f6ffaec3dccb69ad",
+    "url": "/search-react/static/js/runtime~main.2abb16c9.js"
+  }
+]);
